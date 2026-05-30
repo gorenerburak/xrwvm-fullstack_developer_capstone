8a6b89365febbdd15327de0f147b63a07d207b66
gorenerburak <gorenerburak@gmail.com>
2026-05-30 17:19:37 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -21,11 +21,12 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon')
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2026,
+    year = models.IntegerField(
+        default=2026,
         validators=[
             MaxValueValidator(2026),
             MinValueValidator(2015)
         ])
 
     def __str__(self):
-        return self.name# Return the name as the string representation
+        return self.name  # Return the name as the string representation
