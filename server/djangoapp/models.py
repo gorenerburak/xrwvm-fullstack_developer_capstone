0627421e410a6853f0b33da8701d626884e408c1
gorenerburak <gorenerburak@gmail.com>
2026-05-30 16:45:31 +0300

    Update models.py
    Run linter fixing.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,23 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     def __str__(self):
-        return self.name  # Return the name as the string representation
-
+        # Return the name as the string representation
+        return self.name
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
+        ('WAGON', 'Wagon')
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2026,
@@ -27,7 +25,5 @@ class CarModel(models.Model):
             MaxValueValidator(2026),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
-
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name# Return the name as the string representation
